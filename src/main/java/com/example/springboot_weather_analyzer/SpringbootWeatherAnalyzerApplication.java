b4ec55e61e6115d3fbabcfd19af5321d83c3d5dd
Vladimir Zayats <dholl_1987@mail.ru>
2023-11-28 22:08:50 +0300

    Initial commit

---
+++ src/main/java/com/example/springboot_weather_analyzer/SpringbootWeatherAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_weather_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWeatherAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWeatherAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_weather_analyzer/SpringbootWeatherAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_weather_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWeatherAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
